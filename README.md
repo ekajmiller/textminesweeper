9295b68850a2bc1f479a59e358e93c1e1e011a93
Jake E. Miller <jake.e.miller@gmail.com>
2017-06-03 15:20:15 -0500

    Changes namespace from ms_* to tbs_* and updates readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Made while studying for triplebyte interview
+# Text based sweeper (based on Minesweeper)
+
+Made while studying for triplebyte interview.
 
 To compile and run:
 gcc main.c -Wall -Werror && ./a.exe
--- main.c
+++ main.c
@@ -5,134 +5,134 @@
 
 #define ARRAY_SIZE(_ARR) (sizeof(_ARR) / sizeof((_ARR)[0]))
 
-#define MS_ROWS 26
-#define MS_COLS 26
-#define MS_NUM_INDICES (MS_ROWS * MS_COLS)
-#define MS_NUM_MINES (MS_NUM_INDICES / 8)
+#define TBS_ROWS 26
+#define TBS_COLS 26
+#define TBS_NUM_INDICES (TBS_ROWS * TBS_COLS)
+#define TBS_NUM_MINES (TBS_NUM_INDICES / 16)
 
 typedef struct {
     int is_mine; /* We could make minesweeper more fun with
                     an enum and different types of items */
     int visible;
     int proximity; /* How many mines are adjacent? */
-} ms_space_t;
+} tbs_space_t;
 
 typedef enum {
-    MS_TOPLEFT,
-    MS_TOP,
-    MS_TOPRIGHT,
-    MS_RIGHT,
-    MS_BOTRIGHT,
-    MS_BOT,
-    MS_BOTLEFT,
-    MS_LEFT,
-} ms_relpos_t;
+    TBS_TOPLEFT,
+    TBS_TOP,
+    TBS_TOPRIGHT,
+    TBS_RIGHT,
+    TBS_BOTRIGHT,
+    TBS_BOT,
+    TBS_BOTLEFT,
+    TBS_LEFT,
+} tbs_relpos_t;
 
 typedef struct {
-    ms_relpos_t relpos;
+    tbs_relpos_t relpos;
     int relrow;
     int relcol;
-} ms_relpos_def_t;
+} tbs_relpos_def_t;
 
-static ms_space_t ms_board[MS_ROWS][MS_COLS];
+static tbs_space_t tbs_board[TBS_ROWS][TBS_COLS];
 
-static ms_relpos_def_t ms_relpos_defs[] = {
-    { MS_TOPLEFT,  -1, -1, },
-    { MS_TOP,      -1,  0, },
-    { MS_TOPRIGHT, -1,  1, },
-    { MS_RIGHT,     0,  1, },
-    { MS_BOTRIGHT,  1,  1, },
-    { MS_BOT,       1,  0, },
-    { MS_BOTLEFT,   1, -1, },
-    { MS_LEFT,      0, -1, },
+static tbs_relpos_def_t tbs_relpos_defs[] = {
+    { TBS_TOPLEFT,  -1, -1, },
+    { TBS_TOP,      -1,  0, },
+    { TBS_TOPRIGHT, -1,  1, },
+    { TBS_RIGHT,     0,  1, },
+    { TBS_BOTRIGHT,  1,  1, },
+    { TBS_BOT,       1,  0, },
+    { TBS_BOTLEFT,   1, -1, },
+    { TBS_LEFT,      0, -1, },
 };
 
-static int ms_totalnonminehidden;
+static int tbs_totalnonminehidden;
 
-static int ms_quit = 0;
+static int tbs_quit = 0;
 
 
-static int ms_idx2row(int idx)
+static int tbs_idx2row(int idx)
 {
-    return idx / MS_COLS;
+    return idx / TBS_COLS;
 }
 
-static int ms_idx2col(int idx)
+static int tbs_idx2col(int idx)
 {
-    return idx % MS_COLS;
+    return idx % TBS_COLS;
 }
 
-static int ms_rowcol2idx(int row, int col)
+static int tbs_rowcol2idx(int row, int col)
 {
-    if (row < 0 || row >= MS_ROWS || col < 0 || col >= MS_COLS)
+    if (row < 0 || row >= TBS_ROWS || col < 0 || col >= TBS_COLS)
         return -1;
-    return row * MS_COLS + col;
+    return row * TBS_COLS + col;
 }
 
-static int ms_relrowcol2idx(int row, int col, ms_relpos_t relpos)
+static int tbs_relrowcol2idx(int row, int col, tbs_relpos_t relpos)
 {
     /* TODO: sanity check relpos */
-    ms_relpos_def_t *rpd = &ms_relpos_defs[relpos];
-    return ms_rowcol2idx(row + rpd->relrow, col + rpd->relcol);
+    tbs_relpos_def_t *rpd = &tbs_relpos_defs[relpos];
+    return tbs_rowcol2idx(row + rpd->relrow, col + rpd->relcol);
 }
 
-static void ms_populate_board()
+static void tbs_populate_board()
 {
     int ridx, row, col;
-    int randomizer[MS_NUM_INDICES];
+    int randomizer[TBS_NUM_INDICES];
     int minecount;
-    ms_relpos_t relpos;
+    tbs_relpos_t relpos;
 
-    memset(ms_board, 0, sizeof(ms_board));
+    memset(tbs_board, 0, sizeof(tbs_board));
 
     /* Make a list of all of the indices for mine randomization purposes */
-    for (ridx = 0; ridx < MS_NUM_INDICES; ridx++) {
+    for (ridx = 0; ridx < TBS_NUM_INDICES; ridx++) {
         randomizer[ridx] = ridx;
     }
 
     /* Random place mines with help of randomizer list */
-    for (minecount = 0; minecount < MS_NUM_MINES; minecount++) {
+    for (minecount = 0; minecount < TBS_NUM_MINES; minecount++) {
 
         /* Pick random index of randomizer list */
         /* FIXME: correct modulo bias */
-        ridx = rand() % (MS_NUM_INDICES - minecount);
-        row = ms_idx2row(randomizer[ridx]);
-        col = ms_idx2col(randomizer[ridx]);
+        ridx = rand() % (TBS_NUM_INDICES - minecount);
+        row = tbs_idx2row(randomizer[ridx]);
+        col = tbs_idx2col(randomizer[ridx]);
 
         /* Make it a mine! */
-        ms_board[row][col].is_mine = 1;
+        tbs_board[row][col].is_mine = 1;
 
         /* Increment the adjacency counts */
-        for (relpos = 0; relpos < ARRAY_SIZE(ms_relpos_defs); relpos++) {
-            int relidx = ms_relrowcol2idx(row, col, relpos);
+        for (relpos = 0; relpos < ARRAY_SIZE(tbs_relpos_defs); relpos++) {
+            int relidx = tbs_relrowcol2idx(row, col, relpos);
 
             if (relidx < 0) /* Don't go offboard */
                 continue;
 
-            ((ms_space_t*)ms_board)[relidx].proximity++;
+            ((tbs_space_t*)tbs_board)[relidx].proximity++;
         }
 
         /* Remove index from list so it won't be randomized again */
-        randomizer[ridx] = randomizer[MS_NUM_INDICES - minecount - 1];
+        randomizer[ridx] = randomizer[TBS_NUM_INDICES - minecount - 1];
     }
-    ms_totalnonminehidden = MS_NUM_INDICES - MS_NUM_MINES;
+    tbs_totalnonminehidden = TBS_NUM_INDICES - TBS_NUM_MINES;
 }
 
-static void ms_draw_board(int xray)
+static void tbs_draw_board(int xray)
 {
     int row, col;
-    ms_space_t *space;
+    tbs_space_t *space;
 
     printf("  ");
-    for (col = 0; col < MS_COLS; col++) {
+    for (col = 0; col < TBS_COLS; col++) {
         printf("%c ", col + 'A');
     }
     printf("\n");
 
-    for (row = 0; row < MS_ROWS; row++) {
+    for (row = 0; row < TBS_ROWS; row++) {
         printf("%c ", row + 'a');
-        for (col = 0; col < MS_COLS; col++) {
-            space = &ms_board[row][col];
+        for (col = 0; col < TBS_COLS; col++) {
+            space = &tbs_board[row][col];
             if (space->visible || xray) {
                 if (!space->is_mine) {
                     if (space->proximity) {
@@ -153,67 +153,67 @@ static void ms_draw_board(int xray)
     }
 
     printf("  ");
-    for (col = 0; col < MS_COLS; col++) {
+    for (col = 0; col < TBS_COLS; col++) {
         printf("%c ", col + 'A');
     }
     printf("\n");
 }
 
 /* Recursively auto selects all spaces surrounding 0s.  */
-static void ms_auto_select(int row, int col)
+static void tbs_auto_select(int row, int col)
 {
     int idx;
     int relpos;
-    ms_space_t *space;
+    tbs_space_t *space;
 
-    idx = ms_rowcol2idx(row, col);
+    idx = tbs_rowcol2idx(row, col);
 
     /* If invalid space, stop recursing! */
     if (idx < 0)
         return;
 
-    space = &((ms_space_t*)ms_board)[idx];
+    space = &((tbs_space_t*)tbs_board)[idx];
 
     /* If already picked, stop recursing! */
     if (space->visible)
         return;
 
     space->visible = 1;
-    ms_totalnonminehidden--;
+    tbs_totalnonminehidden--;
 
     if (space->proximity != 0) /* Stop recursing if proximity 0 */
         return;
 
     /* Recurse into surrounding areas if proximity is 0 */
-    for (relpos = 0; relpos < ARRAY_SIZE(ms_relpos_defs); relpos++) {
-        ms_auto_select(row + ms_relpos_defs[relpos].relrow,
-                       col + ms_relpos_defs[relpos].relcol);
+    for (relpos = 0; relpos < ARRAY_SIZE(tbs_relpos_defs); relpos++) {
+        tbs_auto_select(row + tbs_relpos_defs[relpos].relrow,
+                       col + tbs_relpos_defs[relpos].relcol);
     }
 }
 
 /* Returns -1 for invalid selection, -2 for mine, -3 for already selected; else proximity */
-static int ms_select(int row, int col)
+static int tbs_select(int row, int col)
 {
     int idx;
-    ms_space_t *space;
+    tbs_space_t *space;
 
-    idx = ms_rowcol2idx(row, col);
+    idx = tbs_rowcol2idx(row, col);
     if (idx < 0)
         return idx;
 
-    space = &((ms_space_t*)ms_board)[idx];
+    space = &((tbs_space_t*)tbs_board)[idx];
 
     if (space->is_mine)
         return -2;
     if (space->visible)
         return -3;
 
-    ms_auto_select(row, col);
+    tbs_auto_select(row, col);
 
     return space->proximity;
 }
 
-static void ms_get_input(char *input, int sz)
+static void tbs_get_input(char *input, int sz)
 {
     char ch;
 
@@ -232,12 +232,12 @@ int main(int argc, char const *argv[])
     int proximity;
 
     srand(time(NULL));
-    ms_populate_board();
+    tbs_populate_board();
 
     printf("Welcome to Text Based Sweeper (TBS)\n\n");
 
-    /* TODO: add signal handler that sets ms_quit to true */
-    while (!ms_quit) {
+    /* TODO: add signal handler that sets tbs_quit to true */
+    while (!tbs_quit) {
         struct {
             const char *label;
             int *fill;
@@ -246,7 +246,7 @@ int main(int argc, char const *argv[])
             { "col", &col },
         };
 
-        ms_draw_board(0);
+        tbs_draw_board(0);
         printf("\n");
 
         for (ii = 0; ii < ARRAY_SIZE(inputs); ii++) {
@@ -255,13 +255,13 @@ int main(int argc, char const *argv[])
             fflush(stdout);
             /* TODO: perhaps combine inputs into one and make it to where
                      you don't have to press enter. */
-            ms_get_input(usr_str, sizeof(usr_str));
+            tbs_get_input(usr_str, sizeof(usr_str));
             /* TODO: make more robust. Check for out of bounds and invalid
                      chars, auto lowercase, etc */
             *(inputs[ii].fill) = usr_str[0] - 'a';
         }
 
-        proximity = ms_select(row, col);
+        proximity = tbs_select(row, col);
         if (proximity == -1) {
             printf("Invalid selection. Choose again.\n\n");
             continue;
@@ -270,7 +270,7 @@ int main(int argc, char const *argv[])
         /* Check lose condition */
         if (proximity == -2) {
             printf("BOOM! Game Over!\n\n");
-            ms_draw_board(1);
+            tbs_draw_board(1);
             break;
         }
 
@@ -281,9 +281,9 @@ int main(int argc, char const *argv[])
         }
 
         /* Check win condition */
-        if (ms_totalnonminehidden <= 0) {
+        if (tbs_totalnonminehidden <= 0) {
             printf("You win!!! Congrats!\n\n");
-            ms_draw_board(1);
+            tbs_draw_board(1);
             break;
         }
 
